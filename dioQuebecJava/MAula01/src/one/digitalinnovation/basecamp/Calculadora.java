de5bcd518bd24abd970635582d461e89f270b799
Fabio Costa <fabio.costta@yahoo.com.br>
2022-11-06 11:27:56 -0300

    Exercicios

---
+++ dioQuebecJava/MAula01/src/one/digitalinnovation/basecamp/Calculadora.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.basecamp;
+W
+/**
+ * Classe de exemplo para o exercicio da Aula 01 de Metodos.
+ */
+public class Calculadora {
+
+    public static void  soma(double numero1, double numero2){
+
+        double resultado= numero1 + numero2;
+
+        System.out.println("A soma de " + numero1 + " mais " + numero2 + " é " + resultado);
+
+    }
+
+}
