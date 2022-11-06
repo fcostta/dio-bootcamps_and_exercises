f64c590cdad326563b6f926101ad0f2730f096d7
Fabio Costa <fabio.costta@yahoo.com.br>
2022-11-06 11:51:27 -0300

    Exercicio1

---
+++ dioQuebecJava/MAula01/src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,27 @@
+package one.digitalinnovation.basecamp;
+
+/**
+ * Classe principal dos exercicios da Aula 1 de Métodos.
+ */
+
+public class Main {
+
+    public static void main(String[] args){
+
+        // Calculadora
+        System.out.println("Exercicio calculadora");
+        Calculadora.soma(3,6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7,8);
+        Calculadora.divisao(5,2.5);
+
+        // Mensagem
+
+
+        // Empréstimo
+
+
+    }
+
+
+}
