79d5e6e1feec50146a99935528319631d52ff12d
Fabio Costa <fabio.costta@yahoo.com.br>
2022-11-06 14:17:47 -0300

    upDate Exercicio 2

--- dioQuebecJava/MAula02/src/one/digitalinnovation/basecamp/Main.java
+++ dioQuebecJava/MAula02/src/one/digitalinnovation/basecamp/Main.java
@@ -12,8 +12,9 @@ public class Main {
         //Quadrilatero
         System.out.println("Exercicio quadrilatero");
         Quadrilatero.area( 3);
-        Quadrilatero.area(5,5);
+        Quadrilatero.area(5d,5d);
         Quadrilatero.area(7,  8,  9);
+        Quadrilatero.area(5f,5f);
     }
 
 
--- dioQuebecJava/MAula02/src/one/digitalinnovation/basecamp/Quadrilatero.java
+++ dioQuebecJava/MAula02/src/one/digitalinnovation/basecamp/Quadrilatero.java
@@ -23,4 +23,10 @@ public class Quadrilatero {
 
         System.out.println("Área do trapézio: " + ((baseMaior+baseMenor)*altura) / 2);
     }
+
+    public static void area(float diagonal1, float diagonal2){
+
+        System.out.println("Área do losangulo: " + (diagonal1 * diagonal2)/2 );
+
+    }
 }
